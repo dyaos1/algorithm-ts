c53f6a51dd2e3efb62a09a7377c01f6c015ab9c9
DESKTOP-AN010CN\Flzomb <spark3840@gmail.com>
2025-02-27 16:11:33 +0900

    hashMap: openAddressing, chaining

--- src/index.ts
+++ src/index.ts
@@ -1,56 +1,13 @@
 // import HashMap from './hashMap/openAddress';
 import HashMap from './hashMap/chaining';
 
-// const arr: [number, string][] = [
-//   [1, 'jack'],
-//   [14, 'john'],
-//   [2, 'hello'],
-//   [4, 'messi'],
-//   [27, 'hali'],
-//   [30, 'hali30'],
-//   [21, 'hali21'],
-// ];
-const hash = new HashMap();
-const arr = [
-  'a',
-  'b',
-  'c',
-  'd',
-  'e',
-  'f',
-  'g',
-  'i',
-  'j',
-  'k',
-  'l',
-  'm',
-  'n',
-  'o',
-  'p',
-  'q',
-  'r',
-];
-console.log(arr.length);
-arr.forEach(e => {
-  console.log(e);
-  hash.add(e, `i-${e}`);
-});
-hash.dumps();
+const hashMap = new HashMap();
 
-// arr.forEach(e => {
-//   hash.add(e[0], e[1]);
-// });
-// hash.dumps();
-// hash.add(22, 'hali22');
-// hash.add(23, 'hali23');
-// hash.dumps();
-// hash.remove(14);
-// hash.remove(23);
-// hash.dumps();
-
-for (let i = 100; i < 200; i++) {
-  hash.add(i, `i-${i}`);
-}
-hash.dumps();
-hash.reHash();
-hash.dumps();
+hashMap.add(36, 'Lee Seungyeop');
+hashMap.add(17, 'Otani Shohei');
+hashMap.add(23, 'Michael Jordan');
+const mj = hashMap.get(23);
+console.log(mj);
+hashMap.remove(23);
+const mjRemoved = hashMap.get(23);
+console.log(mjRemoved);
